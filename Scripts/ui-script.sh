a34d00b69e18595c0a2abfe0a0226159985f716b
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-12-31 13:01:11 +0530

    Update ui-script.sh

--- Scripts/ui-script.sh
+++ Scripts/ui-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERNAME=$(whoami)
-SERVICE_FILE="/etc/systemd/system/ui.service"
+SERVICE_FILE="/etc/systemd/system/ui-display.service"
 
 cat <<EOF | sudo tee $SERVICE_FILE
 [Unit]
@@ -9,10 +9,10 @@ Description=Flask Application with Auto-reload on Changes
 After=network.target
 
 [Service]
-ExecStart=/bin/bash /usr/bin/Chiefnet-OT-Shield/Scripts/start-vite.sh
+ExecStart=/bin/bash /usr/bin/ot-shield-testing/Scripts/start-vite.sh
 Restart=always
 User=$USERNAME
-WorkingDirectory=/usr/bin/Chiefnet-OT-Shield/PythonScript
+WorkingDirectory=/usr/bin/ot-shield-testing/PythonScript
 Environment=PYTHONUNBUFFERED=1
 Environment=PATH=/usr/bin:/usr/local/bin
 StandardOutput=journal
@@ -24,6 +24,6 @@ EOF
 
 # Reload systemd and start the service
 sudo systemctl daemon-reload
-sudo systemctl enable ui.service
-sudo systemctl start ui.service
-sudo systemctl restart ui.service
+sudo systemctl enable ui-display.service
+sudo systemctl start ui-display.service
+sudo systemctl restart ui-display.service
