701bd57b6ca26653ed5fd54e80548a8670ccd786
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2024-12-31 14:42:10 +0530

    Update start-service.sh

--- start-service.sh
+++ start-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set executable permissions for the scripts
-sudo chmod +x /usr/bin/ot-shield-testing/Scripts/arp-script.sh
+sudo chmod +x /usr/bin/ot-shield-testing/Scripts/arp-service.sh
 sudo chmod +x /usr/bin/ot-shield-testing/Scripts/network-configuration-service.sh
 sudo chmod +x /usr/bin/ot-shield-testing/Scripts/start-vite.sh
 sudo chmod +x /usr/bin/ot-shield-testing/Scripts/ui-script.sh
@@ -13,7 +13,7 @@ cd /usr/bin/ot-shield-testing/Scripts/
 
 # Run each script in sequence
 echo "Running arp_script.sh..."
-sudo ./arp-script.sh
+sudo ./arp-service.sh
 
 echo "Running network-configuration-service.sh..."
 sudo ./network-configuration-service.sh
@@ -25,8 +25,7 @@ echo "Running ui-script.sh..."
 sudo ./ui-script.sh
 
 echo "Running default-config-script.sh..."
-sudo ./default-config-script.sh
+sudo ./default-config-service.sh
 
 echo "All scripts executed successfully."
-
 
