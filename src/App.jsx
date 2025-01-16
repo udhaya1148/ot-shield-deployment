19cb80f734326e3b632bb7f03cd44e4614d90e1b
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-01-16 10:46:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-
 import MainDashboard from "./components/MainDashboard";
 import NetworkConfiguration from "./components/NetworkConfiguration";
 import ArpTable from "./components/ArpTable";
@@ -8,19 +7,17 @@ import DeleteArp from "./components/DeleteArp";
 import RoutesTable from "./components/RoutesTable";
 import Terminal from "./components/Terminal";
 import LoginPage from "./components/LoginPage";
+import { IoMdLogOut } from "react-icons/io";
 
 function Header() {
-  const navigate = useNavigate(); // Hook for navigation
+  const navigate = useNavigate(); 
 
   const handleLogout = () => {
-    // Handle any logout logic here, e.g., clearing session, tokens, etc.
-    console.log("Logged out");
-    // Redirect to the login page
     navigate("/");
   };
 
   return (
-    <h1 className="font-bold text-3xl h-14 bg-gray-300 w-full p-2 flex items-center justify-between">
+    <h1 className="font-bold text-3xl h-14 bg-gray-300 w-screen p-2 flex items-center justify-between">
       <div className="flex items-center">
         <img
           src="https://chiefnet.io/wp-content/uploads/2022/08/Chiefnet-logo-5.svg"
@@ -30,13 +27,15 @@ function Header() {
         OT Shield
       </div>
 
-      {/* Logout button positioned to the right */}
-      <button
+      {location.pathname !=="/" && (
+        <button
         onClick={handleLogout}
-        className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
+        className="bg-red-500 text-white rounded-md hover:bg-red-600 mr-4"
       >
-        Logout
+        <IoMdLogOut />
       </button>
+      )}
+      
     </h1>
   );
 }
