868d84030143344e1219f291ccac9fb9f380158a
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-01-17 16:44:41 +0530

    Update network-configuration-service.sh

--- Scripts/network-configuration-service.sh
+++ Scripts/network-configuration-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERNAME=$(whoami)
-SERVICE_FILE="/etc/systemd/system/network-edit.service"
+SERVICE_FILE="/etc/systemd/system/network-configuration.service"
 
 cat <<EOF | sudo tee $SERVICE_FILE
 [Unit]
@@ -9,10 +9,10 @@ Description=Flask Application with Auto-reload on Changes
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 /usr/bin/ot-shield-testing/PythonScript/Network-configuration.py
+ExecStart=/usr/bin/python3 /usr/bin/Chiefnet-OT-Shield/PythonScript/Network-configuration.py
 Restart=always
 User=$USERNAME
-WorkingDirectory=/usr/bin/ot-shield-testing/PythonScript
+WorkingDirectory=/usr/bin/Chiefnet-OT-Shield/PythonScript
 Environment=PYTHONUNBUFFERED=1
 Environment=PATH=/usr/bin:/usr/local/bin
 StandardOutput=journal
@@ -24,6 +24,6 @@ EOF
 
 # Reload systemd and start the service
 sudo systemctl daemon-reload
-sudo systemctl enable network-edit.service
-sudo systemctl start network-edit.service
-sudo systemctl restart network-edit.service
+sudo systemctl enable network-configuration.service
+sudo systemctl start network-configuration.service
+sudo systemctl restart network-configuration.service
