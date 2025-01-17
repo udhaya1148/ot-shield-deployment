bea921e77712e10b61256cfa6602e27f40b8f824
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-01-17 16:51:51 +0530

    Update start-service.sh

--- start-service.sh
+++ start-service.sh
@@ -2,7 +2,7 @@
 
 echo "Permissions updated for all scripts."
 
-cd /usr/bin/ot-shield-testing/Scripts/
+cd /usr/bin/Chiefnet-OT-Shield/Scripts/
 
 # Run each script in sequence
 echo "Running arp_script.sh..."
@@ -12,10 +12,10 @@ echo "Running network-configuration-service.sh..."
 sudo ./network-configuration-service.sh
 
 echo "Running routes-script.sh..."
-sudo ./routes-script.sh
+sudo ./routes-service.sh
 
 echo "Running ui-script.sh..."
-sudo ./ui-script.sh
+sudo ./ui-service.sh
 
 echo "Running default-config-script.sh..."
 sudo ./default-config-service.sh
@@ -23,6 +23,8 @@ sudo ./default-config-service.sh
 echo "Running terminal.sh..."
 sudo ./terminal-service.sh
 
+echo "Running terminal.sh..."
+sudo ./login-service.sh
 
 echo "All scripts executed successfully."
 
