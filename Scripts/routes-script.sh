297244f252746e0b273b94613265cb02362b8465
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-01-17 16:44:57 +0530

    Update routes-script.sh

--- Scripts/routes-script.sh
+++ Scripts/routes-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERNAME=$(whoami)
-SERVICE_FILE="/etc/systemd/system/routes-display.service"
+SERVICE_FILE="/etc/systemd/system/routes.service"
 
 cat <<EOF | sudo tee $SERVICE_FILE
 [Unit]
@@ -9,10 +9,10 @@ Description=Flask Application with Auto-reload on Changes
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 /usr/bin/ot-shield-testing/PythonScript/routes.py
+ExecStart=/usr/bin/python3 /usr/bin/Chiefnet-OT-Shield/PythonScript/routes.py
 Restart=always
 User=$USERNAME
-WorkingDirectory=/usr/bin/ot-shield-testing/PythonScript
+WorkingDirectory=/usr/bin/Chiefnet-OT-Shield/PythonScript
 Environment=PYTHONUNBUFFERED=1
 Environment=PATH=/usr/bin:/usr/local/bin
 StandardOutput=journal
@@ -24,6 +24,6 @@ EOF
 
 # Reload systemd and start the service
 sudo systemctl daemon-reload
-sudo systemctl enable routes-display.service
-sudo systemctl start routes-display.service
-sudo systemctl restart routes-display.service
+sudo systemctl enable routes.service
+sudo systemctl start routes.service
+sudo systemctl restart routes.service
