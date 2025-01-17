d3e19fbac91c8b1d3993b0737ec3ebad535dc36a
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-01-17 16:43:42 +0530

    Update arp-service.sh

--- Scripts/arp-service.sh
+++ Scripts/arp-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERNAME=$(whoami)
-SERVICE_FILE="/etc/systemd/system/arp-display.service"
+SERVICE_FILE="/etc/systemd/system/arp.service"
 
 cat <<EOF | sudo tee $SERVICE_FILE
 [Unit]
@@ -9,8 +9,8 @@ Description=Flask Application with Auto-reload on Changes
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 /usr/bin/ot-shield-testing/PythonScript/arp-pythonscript.py
-WorkingDirectory=/usr/bin/ot-shield-testing/PythonScript
+ExecStart=/usr/bin/python3 /usr/bin/Chiefnet-OT-Shield/PythonScript/arp-pythonscript.py
+WorkingDirectory=/usr/bin/Chiefnet-OT-Shield/PythonScript
 User=$USERNAME
 Restart=always
 
@@ -20,6 +20,6 @@ EOF
 
 # Reload systemd and start the service
 sudo systemctl daemon-reload
-sudo systemctl enable arp-display.service
-sudo systemctl start arp-display.service
-sudo systemctl restart arp-display.service
+sudo systemctl enable arp.service
+sudo systemctl start arp.service
+sudo systemctl restart arp.service
