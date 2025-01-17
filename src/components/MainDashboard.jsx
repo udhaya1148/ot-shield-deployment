87bc1a6afa343bf84bf304a27e76964bd30bbe0d
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-01-17 12:46:59 +0530

    Update MainDashboard.jsx

--- src/components/MainDashboard.jsx
+++ src/components/MainDashboard.jsx
@@ -1,42 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import SideMenu from "./SideMenu";
 
 function MainDashboard() {
-  const [grafanaUrl, setGrafanaUrl] = useState("");
-
   useEffect(() => {
-    // Fetch the dynamically detected Grafana URL from the backend
-    const fetchGrafanaIp = async () => {
-      try {
-        const response = await fetch("/api/grafana-ip");
-        if (response.ok) {
-          const data = await response.json();
-          setGrafanaUrl(data.grafana_ip);
-        } else {
-          console.error("Failed to fetch Grafana IP");
-        }
-      } catch (error) {
-        console.error("Error fetching Grafana IP:", error);
-      }
-    };
+    // Disable scrolling when the component is mounted
+    document.body.style.overflowX = "hidden";
+    document.body.style.overflowY = "auto";
 
-    fetchGrafanaIp();
+    // Enable scrolling when the component is unmounted
+    return () => {
+      document.body.style.overflowX = "auto";
+      document.body.style.overflowY = "auto";
+
+    };
   }, []);
 
   return (
     <div className="flex flex-row h-screen w-screen">
       <SideMenu />
-      <div className="flex-grow p-1 overflow-auto justify-center">
-        {grafanaUrl ? (
-          <iframe
-            src={grafanaUrl}
-            frameBorder="0"
-            title="Dashboard"
-            className="w-full h-full"
-          ></iframe>
-        ) : (
-          <p>Loading Grafana dashboard...</p>
-        )}
+      <div className="flex-grow p-1 overflow-hidden justify-center">
+        <iframe
+          src={`http://${window.location.hostname}:3000/d/de72jamksx14wa/network?orgId=1&from=2025-01-16T11:17:44.233Z&to=2025-01-16T11:32:44.234Z&timezone=Asia%2FKolkata&refresh=auto`}
+          frameBorder="0"
+          title="Dashboard"
+          className="w-full h-full"
+          style={{ objectFit: "contain" }}  // Ensures the iframe content fits the page without overflow
+        ></iframe>
       </div>
     </div>
   );
