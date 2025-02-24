5b51e1efec43a974ba3153c654a7fe849cd7b7ae
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-02-24 16:30:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,15 @@ import { IoMdLogOut } from "react-icons/io";
 import IPAddress from "./components/IPAddress";
 import Routing from "./components/Routing";
 import Gateways from "./components/Gateways";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function Header() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
+    sessionStorage.clear(); // Clears authentication state
     navigate("/");
-  };
+  };  
 
   return (
     <h1 className="font-bold text-3xl h-14 bg-gray-300 w-screen p-2 flex items-center justify-between">
@@ -51,25 +53,46 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<MainDashboard />} /> 
-            <Route path="/network-configuration" element={<NetworkConfiguration />} />
+            <Route
+              path="/home"
+              element={<ProtectedRoute element={<MainDashboard />} />}
+            />
+            <Route
+              path="/network-configuration"
+              element={<ProtectedRoute element={<NetworkConfiguration />} />}
+            />
             <Route
               path="/ip-address"
-              element={<IPAddress />}
+              element={<ProtectedRoute element={<IPAddress />} />}
             />
             <Route
               path="/gateway"
-              element={<Gateways />}
+              element={<ProtectedRoute element={<Gateways />} />}
             />
             <Route
               path="/routing"
-              element={<Routing />}
+              element={<ProtectedRoute element={<Routing />} />}
             />
-            <Route path="/arp" element={<ArpTable />} />
-            <Route path="/add-arp" element={<AddStaticArp />} />
-            <Route path="/delete-arp" element={<DeleteArp />} />
-            <Route path="/routes" element={<RoutesTable />} />
-            <Route path="/terminal" element={<Terminal />} />
+            <Route
+              path="/arp"
+              element={<ProtectedRoute element={<ArpTable />} />}
+            />
+            <Route
+              path="/add-arp"
+              element={<ProtectedRoute element={<AddStaticArp />} />}
+            />
+            <Route
+              path="/delete-arp"
+              element={<ProtectedRoute element={<DeleteArp />} />}
+            />
+            <Route
+              path="/routes"
+              element={<ProtectedRoute element={<RoutesTable />} />}
+            />
+            <Route
+              path="/terminal"
+              element={<ProtectedRoute element={<Terminal />} />}
+            />
           </Routes>
         </div>
       </div>
