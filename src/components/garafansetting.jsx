958e61a98ba28fb8c03b3389e7c19b0e6cc40926
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-03-17 14:51:34 +0530

    Update garafansetting.jsx

--- src/components/garafansetting.jsx
+++ src/components/garafansetting.jsx
@@ -18,44 +18,53 @@ function MainDashboard() {
   ];
 
   const panelSizes = {
-    9: { width: 1200, height: 900 }, // Increased size
-    8: { width: 900, height: 900 },
-    10: { width: 500, height: 400 },
-    11: { width: 163, height: 104 },
-    12: { width: 162, height: 105 },
-    13: { width: 400, height: 300 }, // Increased size
-    14: { width: 800, height: 200 }, // Increased size
-    21: { width: 400, height: 150 }, // Increased size
-    23: { width: 400, height: 150 }, // Increased size
-    22: { width: 400, height: 150 }, // Increased size
-    24: { width: 400, height: 150 }, // Increased size
+    9: { width: 300, height: 300 }, // cpu usage
+    13: { width: 300, height: 300 }, // memory
+    14: { width: 750, height: 200 }, // proces status
+
+    8: { width: 267, height: 109 }, //system up time
+    10: { width: 267, height: 109 }, // disk total
+    11: { width: 267, height: 109 }, //disk used
+    12: { width: 267, height: 109 }, //disk free
+
+    // Temperature
+    21: { width: 600, height: 150 }, 
+    23: { width: 600, height: 150 },
+    22: { width: 600, height: 150 },
+    24: { width: 600, height: 150 },
+
     1: { width: 267, height: 109 },
     3: { width: 267, height: 109 },
     4: { width: 267, height: 109 },
     5: { width: 267, height: 109 },
     6: { width: 267, height: 109 },
+
     20: { width: 267, height: 109 },
     25: { width: 267, height: 109 },
     26: { width: 267, height: 109 },
     27: { width: 267, height: 109 },
     28: { width: 267, height: 109 },
+
     15: { width: 267, height: 109 },
     16: { width: 267, height: 109 },
     17: { width: 267, height: 109 },
     18: { width: 267, height: 109 },
     19: { width: 267, height: 109 },
+
     31: { width: 267, height: 109 },
     32: { width: 267, height: 109 },
     34: { width: 267, height: 109 },
     36: { width: 267, height: 109 },
     38: { width: 267, height: 109 },
+
     33: { width: 267, height: 109 },
     35: { width: 267, height: 109 },
     37: { width: 267, height: 109 },
     39: { width: 267, height: 109 },
     40: { width: 267, height: 109 },
-    30: { width: 900, height: 200 }, // Increased size
-    41: { width: 700, height: 200 }, // Increased size
+
+    30: { width: 800, height: 150 }, // static arp status
+    41: { width: 700, height: 150 }, // dd to it speed
   };
 
   // Default size for panels not explicitly listed in panelSizes
@@ -65,9 +74,9 @@ function MainDashboard() {
   const panelPositions = {
     9: { gridColumn: "1", gridRow: "1" },
 
-    13: { gridColumn: "2", gridRow: "1" },
+    13: { gridColumn: "5", gridRow: "1" },
 
-    14: { gridColumn: "2 / span 3", gridRow: "2" },
+    14: { gridColumn: "2 /span 3", gridRow: "1" },
 
     8: { gridColumn: "1", gridRow: "3" },
     10: { gridColumn: "2", gridRow: "3" },
@@ -75,9 +84,9 @@ function MainDashboard() {
     12: { gridColumn: "4", gridRow: "3" },
 
     21: { gridColumn: "1 /span 2", gridRow: "4" },
-    23: { gridColumn: "3 /span 2 ", gridRow: "4" },
+    23: { gridColumn: "3 /span 4 ", gridRow: "4" },
     22: { gridColumn: "1 / span 2", gridRow: "5" },
-    24: { gridColumn: "3 /span 2", gridRow: "5" },
+    24: { gridColumn: "3 /span 4", gridRow: "5" },
 
     1: { gridColumn: "1", gridRow: "6" },
     3: { gridColumn: "2", gridRow: "6" },
@@ -109,8 +118,8 @@ function MainDashboard() {
     39: { gridColumn: "4", gridRow: "10" },
     40: { gridColumn: "5", gridRow: "10" },
 
-    30: { gridColumn: "1 / span 3", gridRow: "11" },
-    41: { gridColumn: "4 / span 2", gridRow: "11" },
+    30: { gridColumn: "1 /span 3", gridRow: "11" },
+    41: { gridColumn: "4 /span 5 ", gridRow: "11" },
   };
 
   const dashboardUrl = `http://${window.location.hostname}:3000/d-solo/de72jamksx14wa/network?orgId=1&timezone=Asia%2FKolkata&refresh=auto&theme=light`;
@@ -119,9 +128,10 @@ function MainDashboard() {
     <div className="flex flex-row h-screen w-screen">
       <SideMenu />
       <div
-        className="flex-grow p-1 overflow-auto grid gap-4 auto-rows-min"
+        className="flex-grow p-1 overflow-auto overflow-x-hidden grid gap-4"
         style={{
-          gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
+          gridTemplateColumns: "repeat(5, 1fr)",
+          gridAutoRows: "min-content",
         }}
       >
         {panelIds.map((panelId, index) => {
@@ -131,16 +141,20 @@ function MainDashboard() {
           return (
             <div
               key={index}
-              style={{ gridColumn, gridRow }}
-              className="w-full h-full"
+              style={{
+                gridColumn,
+                gridRow,
+                maxWidth: "100%", 
+              }}
+              className="flex justify-center items-center"
             >
-              {index < 5 ? ( // Preload the first 5 panels
+              {index < 5 ? ( 
                 <iframe
                   src={`${dashboardUrl}&panelId=${panelId}`}
                   width={width}
                   height={height}
                   frameBorder="0"
-                  className="w-full h-full"
+                  className="max-w-full"
                 ></iframe>
               ) : (
                 <LazyLoad height={height} offset={100}>
@@ -149,7 +163,7 @@ function MainDashboard() {
                     width={width}
                     height={height}
                     frameBorder="0"
-                    className="w-full h-full"
+                    className="max-w-full"
                   ></iframe>
                 </LazyLoad>
               )}
