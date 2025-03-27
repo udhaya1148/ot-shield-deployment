b27ee764cd3893dafc3a97972e843adb24a1e616
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-03-27 15:17:36 +0530

    Update RoutesTable.jsx

--- src/components/RoutesTable.jsx
+++ src/components/RoutesTable.jsx
@@ -36,27 +36,27 @@ function RoutesTable() {
   
 
   return (
-    <div className="flex flex-row h-screen w-screen">
+    <div className="flex flex-row h-screen w-screen bg-gray-100">
       <SideMenu />
-      <div className="flex-grow p-5 overflow-auto mt-4 justify-center">
-        <div className="border border-black mb-2 p-3 bg-white rounded-lg shadow-lg">
+      <div className="flex-grow p-5 overflow-auto mt-4 justify-center bg-gray-100">
+        <div className=" bg- white mb-2 p-3 bg-white rounded-lg shadow-lg mr-4">
          
             <div>
-              <h3 className="text-2xl text-green-600 font-bold mt-6">
+              <h3 className="text-2xl text-black font-bold mt-1">
                 Route Table
               </h3>
-              <div className="flex items-center justify-between mt-4">
-                <div className="font-bold flex-1">Metric</div>
-                <div className="font-bold flex-1">Genmask</div>
-                <div className="font-bold flex-1">Destination</div>
-                <div className="font-bold flex-1">Gateway</div>
-                <div className="font-bold flex-1">Interface</div>
-                <div className="font-bold flex-1">Flags</div>
+              <div className="grid grid-cols-6 text-center bg-gray-200 justify-between p-2 mt-3 rounded-lg ">
+                <div>Metric</div>
+                <div>Genmask</div>
+                <div>Destination</div>
+                <div>Gateway</div>
+                <div>Interface</div>
+                <div>Flags</div>
               </div>
               {routes.map((route, index) => (
                 <div
                   key={index}
-                  className="flex items-center border border-black justify-between bg-gray-100 p-2 mb-2 rounded-lg"
+                  className="grid grid-cols-6 items-center text-center justify-between bg-gray-100 p-2 mb-2 mt-2 rounded-lg"
                 >
                   <div className="flex-1">{route.Metric ?? "-"}</div>
                   <div className="flex-1">{route.Genmask ?? "-"}</div>
