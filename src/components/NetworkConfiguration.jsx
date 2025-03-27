1e88ad1d07fb80fc815c11f55557b2c1231036af
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-03-27 15:54:14 +0530

    Update NetworkConfiguration.jsx

--- src/components/NetworkConfiguration.jsx
+++ src/components/NetworkConfiguration.jsx
@@ -31,19 +31,19 @@ function NetworkConfiguration() {
     <div className="flex flex-row h-screen w-screen">
       <SideMenu />
       <div className="flex-1 flex flex-col bg-gray-100">
-        <div className="flex bg-white shadow">
+        <div className="flex bg-white shadow item-center rounded-lg mr-7 ml-7 mt-4">
           {tabs.map((tab, index) => (
             <button
               key={index}
               onClick={() => setActiveTab(tab.name)}
               className={`relative px-6 py-3 font-medium border-b-2 transition-all 
-              ${activeTab === tab.name ? "border-blue-500 text-blue-600" : "border-transparent text-gray-600"}
-              hover:text-blue-500`}
+              ${activeTab === tab.name ? "border-teal-400 text-teal-400" : "border-transparent text-black"}
+              hover:text-teal-400`}
             >
               {tab.name}
               {activeTab === tab.name && (
                 <span
-                  className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-4 h-4 bg-blue-500 rotate-0"
+                  className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-4 h-4 bg-teal-400 rotate-0"
                   style={{ clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)" }}
                 ></span>
               )}
