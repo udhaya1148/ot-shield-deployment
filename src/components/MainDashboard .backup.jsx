f93c8431c8c5eb3f47bafd5311cba36342aec3c3
udhaya1148 <59645398+udhaya1148@users.noreply.github.com>
2025-03-27 13:10:34 +0530

    Update MainDashboard .backup.jsx

--- src/components/MainDashboard .backup.jsx
+++ src/components/MainDashboard .backup.jsx
@@ -15,15 +15,15 @@ function MainDashboard() {
   }, []);
 
   return (
-    <div className="flex flex-row h-screen w-screen">
+    <div className="flex flex-row h-screen w-screen bg-gray-100">
       <SideMenu />
       <div className="flex-grow p-1 overflow-hidden flex justify-center relative">
-        <div className="w-full h-full relative">
+        <div className="w-full h-full bg-gray-100 flex items-center justify-center">
           <iframe
             src={`http://${window.location.hostname}:3000/d/de72jamksx14wa/network?orgId=1&from=2025-03-25T10:52:31.142Z&to=2025-03-25T11:07:31.142Z&timezone=Asia%2FKolkata&refresh=auto&theme=light&kiosk=1`}
             frameBorder="0"
             title="Dashboard"
-            className="w-full h-full absolute top-[-60px] left-0"
+            className="w-full h-full absolute top-[-60px]"
           ></iframe>
         </div>
       </div>
